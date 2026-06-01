04a30695b27bbe8213a6b74e63c69bb9259ff886
subham-haldar <subham7software@gmail.com>
2026-06-02 01:09:01 +0530

    Add solution for validating a palindrome with one character removal

---
+++ STRINGS-2/!!!!_VALID_PALINDROME_2_.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+class solution {
+public :
+    bool validpalindrome(string s) {
+        int l = 0 ; 
+        int r = s.length()-1 ; 
+        while(l<r){
+            if(s[l]!=s[r]) return false ;
+            l++ ; 
+            r-- ; 
+        }
+        return true ; 
+    }
+
+    bool isplaindrome(string s){ // return {value 1 , value 2} ; 
+    int i = 0 ; 
+    int j = s.length()-1 ; 
+    while(i<j){
+        if(s[i]!=s[j]) {
+           string s1 = s.substr(0,i) + s.substr(i+1) ; // skip the i pointer
+           string s2 = s.substr(0,j) + s.substr(j+1) ; // skip the j pointer
+           return validpalindrome(s1) || validpalindrome(s2) ;
+        }
+        i++ ; 
+        j-- ; 
+     }
+     return true ; 
+    }
+} ;
