31d844d210abdb357106a8cb3b67e965c7940b43
subham-haldar <subham7software@gmail.com>
2026-06-03 01:16:52 +0530

    .

--- STRINGS-3/IS_SUBSEQUENCE.cpp
+++ STRINGS-3/IS_SUBSEQUENCE.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std ; 
 class Solution {
-public:
+public:       // THINK LIKE A CHECKLIST where in s you have to complete it in order 
+             // untill you do not match s[i] & s[j] move j forward 
     bool isSubsequence(string s, string t) {
            int i = 0 ; 
            int j = 0 ; 
