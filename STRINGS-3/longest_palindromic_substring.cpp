96d25345fe707e892987ec07a20e96c9929c7bb0
subham-haldar <subham7software@gmail.com>
2026-06-02 22:27:31 +0530

    .

---
+++ STRINGS-3/longest_palindromic_substring.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+// OPTIMAL SOLUTION , TC - O(N*N)  SC = O(2n) storing ans and storing in words 
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string ans = s.substr(0,1);
+        if(s.length()<2) return s ; 
+        for(int i =1 ; i<s.length(); i++){
+
+
+        // odd length palindromic substring 
+        int left = i ; 
+        int right = i ; 
+        while(s[left]==s[right]) {
+            left-- ; 
+            right++ ;
+            if(left==-1 || right==s.length()) break ; // check for boundary condition 
+        }
+        // extract the substring
+        string word = s.substr(left+1,right-left-1) ; 
+        if(word.length()>ans.length()) ans = word ; 
+
+
+           // even length string 
+            left = i-1 ; 
+            right =  i ; 
+           while(s[left]==s[right]) {
+            left--;
+            right++ ; 
+            if(left==-1 || right == s.length()) break ; 
+           }
+           string len = s.substr(left+1,right-left-1) ; 
+            if(len.length() >ans.length() ) ans = len ; 
+       }
+       return ans ; 
+      
+    }
+};
+
+// BRUTE FORCE , TC = O(N*N*N)  SC = O(2n)
+// generate all substring and compare for palindrome
